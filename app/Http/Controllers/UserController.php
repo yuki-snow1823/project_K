8e01b233abcbd9191268512f75e6a9e5dea0a5b6
yuki-snow1823 <magic.iroha@gmail.com>
2020-08-16 16:24:23 +0900

    fix:表示できないバグ修正

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\UserController; // これを追加したけどダメ
 
 class UserController extends Controller
 {
     public function mypage()
     {
-        return view('mypage');
+        return view('/user/mypage');
     }
 }
